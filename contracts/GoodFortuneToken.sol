1fc494a23a131c94fb2b5ae4a78646b1d9c58474
luckyfinance-dev <78893395+luckyfinance-dev@users.noreply.github.com>
2021-02-23 15:51:27 -0800

    Renamed Token Variables

--- contracts/GoodFortuneToken.sol
+++ contracts/GoodFortuneToken.sol
@@ -2,8 +2,8 @@ pragma solidity 0.6.12;
 
 import "./libs/BEP20.sol";
 
-// EggToken with Governance.
-contract EggToken is BEP20('Good Fortune Token', 'GFT') {
+// GoodFortuneToken with Governance.
+contract GoodFortuneToken is BEP20('Good Fortune Token', 'GFT') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
